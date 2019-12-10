9d6a5da86f4e02498b67cce2e59d732782008775
hyeonju <tangled6648@gmail.com>
2019-12-10 13:18:27 +0900

    20191210_2

--- models/item.js
+++ models/item.js
@@ -2,20 +2,22 @@ var mongoose = require('mongoose'),
     mongoosePaginate = require('mongoose-paginate'),
     Schema = mongoose.Schema;
 
+//여행지, 이름, 가격, 설명, 여행 코스
 var schema = new Schema({
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
-  title: {type: String, trim: true, required: true},
+  author: { type: Schema.Types.ObjectId, ref: 'Guide' },
+  cityName: {type: String, trim: true, required: true},
+  tourTitle: {type: String, trim: true, required: true},
   content: {type: String, trim: true, required: true},
-  tags: [String],
-  numLikes: {type: Number, default: 0},
-  numAnswers: {type: Number, default: 0},
-  numReads: {type: Number, default: 0},
+  tourCourse: {type: String, trim: true, required: true},
   createdAt: {type: Date, default: Date.now}
+  //numLikes: {type: Number, default: 0},
+  //numAnswers: {type: Number, default: 0},
+  //numReads: {type: Number, default: 0},
 }, {
   toJSON: { virtuals: true},
   toObject: {virtuals: true}
 });
 schema.plugin(mongoosePaginate);
-var Question = mongoose.model('Question', schema);
+var Question = mongoose.model('item', schema);
 
-module.exports = Question;
+module.exports = item;
